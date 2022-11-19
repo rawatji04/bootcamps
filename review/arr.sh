a9810d24408489ccfc152d6c1434203916aa80ec
rawatji04 <narenderjob04@gmail.com>
2022-11-19 19:57:00 +0530

    first commit

---
+++ review/arr.sh
@@ -0,0 +1,8 @@
+players=("dhoni" "kohli" "rohit" "rahul")
+echo "the array is ${players[11]}"
+
+players[4]=chahal
+players[5]=jadeja
+
+echo "after add, the array is ${players[@]}"
+
---
+++ review/stage.sh
@@ -0,0 +1,9 @@
+read -p "enter a player in ipl" player
+
+        if [$player=="Dhoni"]
+
+        then
+
+           echo "he is captain of csk"
+
+        fi
